ec4a9de51460609e42bca431dffa757844d23b05
Fischer Matthias <Matthias.Fischer@doubleSlash.de>
2018-12-05 20:31:18 +0100

    Add hardcoded user and admin credentials with BCrypt encoding

--- src/main/java/hello/WebSecurityConfig.java
+++ src/main/java/hello/WebSecurityConfig.java
@@ -1,9 +1,15 @@
 package hello;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
@@ -20,7 +26,36 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
             .logout()
                 .permitAll();
+    }
+    
+    @Bean
+    public UserDetailsService userDetailsService() {
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+        
+        createUser(manager);
+        createAdminUser(manager);
+        
+        return manager;
+    }
 
+    private void createUser(InMemoryUserDetailsManager manager) {
+    	manager.createUser(User
+    			.withUsername("user")
+    			.password(encoder().encode("userPass"))
+    			.roles("USER").build());
+    }
+
+	private void createAdminUser(InMemoryUserDetailsManager manager) {
+		manager.createUser(User
+          .withUsername("admin")
+          // password "admin" already encoded with BCrypt:
+          .password("$2a$10$tereo6Y/sLTECpBEFrKEqeUe1XveQVQONKxEpIygO4eTcqOKyuebS")
+          .roles("ADMIN").build());
+	}
+    
+    @Bean
+    public PasswordEncoder encoder() {
+        return new BCryptPasswordEncoder();
     }
 
 }
